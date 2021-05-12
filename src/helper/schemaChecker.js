b49b8a4a9ca971b806e00ffbc2375b2e55e5c4ed
Bernhard Maier <mail@bernhard-maier.net>
2021-05-12 13:32:27 +0200

    Fixed several issues in index.js and helper methods

--- src/helper/schemaChecker.js
+++ src/helper/schemaChecker.js
@@ -4,8 +4,14 @@ module.exports = async (variable, variableName, schema) => {
   return new Promise((resolve, reject) => {
     (async () => {
       try {
-        if (schema !== undefined && schema.check(variable)[0]) {
-          reject(`Variable "${variableName}" is invalid: ${schema.check(variable)[0].message}`);
+        if (schema !== undefined) {
+          if (typeof schema.check !== 'function') {
+            return reject(`Given schema for "${variableName}" does not have a check function!`);
+          }
+
+          if (schema.check(variable)[0]) {
+            return reject(`Variable "${variableName}" is invalid: ${schema.check(variable)[0].message}`);
+          }
         }
 
         resolve();

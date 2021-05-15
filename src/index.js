649d3044fc9d4a74d4359f6eddbec590603ce8f5
Bernhard Maier <mail@neopheus.de>
2021-05-15 08:55:33 +0200

    Simplified start index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 'use strict';
 
-const functions = require('./functions');
-const functionExports = {};
-const functionKeys = Object.keys(functions);
-let functionKey;
-
-for (let i = 0; i < functionKeys.length; i++) {
-  functionKey = functionKeys[i];
-  functionExports[functionKey] = functions[functionKey];
-}
-
-module.exports = functionExports;
+module.exports = require('./functions');
